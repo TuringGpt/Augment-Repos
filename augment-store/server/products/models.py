9f60096dd9daf090c87e34aced168b9d8b5fa20f
sagelite <sageliteoff@gmail.com>
2025-10-20 16:40:20 +0000

    AS-5 - set image field to null when storage file is delted

--- augment-store/server/products/models.py
+++ augment-store/server/products/models.py
@@ -9,7 +9,7 @@ class ProductBrand(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_brands')
-    image = models.ForeignKey(File, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ForeignKey(File, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -20,7 +20,7 @@ class ProductCategory(MPTTModel, BaseModel):
     description = models.TextField(null=True, blank=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_categories')
-    image = models.ForeignKey(File, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ForeignKey(File, on_delete=models.SET_NULL, null=True, blank=True)
     class MPTTMeta:
         order_insertion_by = ['name']
 
