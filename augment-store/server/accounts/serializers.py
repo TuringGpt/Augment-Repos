7194c2cd66ddd05793423182026092ff9a01878e
sagelite <sageliteoff@gmail.com>
2025-10-21 08:11:05 +0000

    AS-5 - implement account endpoint and test cases

---
+++ augment-store/server/accounts/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer for retrieving user profile information"""
+    full_name = serializers.CharField(read_only=True)
+    is_registration_completed = serializers.BooleanField(read_only=True)=
+    
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "full_name",
+            "mobile",
+            "gender",
+            "image",
+            "role",
+            "is_active",
+            "is_registration_completed",
+            "date_joined",
+        ]
+        read_only_fields = ["id", "email", "role", "is_active", "date_joined"]
+
+
+class UpdateUserProfileSerializer(serializers.ModelSerializer):
+    """Serializer for updating user profile information"""
+    
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "mobile",
+            "gender",
+            "image",
+        ]
+    
+    def validate_mobile(self, value):
+        """Validate mobile number format"""
+        if value and len(value) > 20:
+            raise serializers.ValidationError("Mobile number is too long")
+        return value
+
