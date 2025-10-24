b14c57b8dced321fc288653bda846ef82c58487a
sagelite <sageliteoff@gmail.com>
2025-10-24 01:43:27 +0000

    SCRUM-32 - implement cart and cart item table

--- augment-store/server/carts/models.py
+++ augment-store/server/carts/models.py
@@ -2,19 +2,20 @@ from django.db import models
 from core.models import BaseModel
 from accounts.models import User
 from products.models import Product
+from django.db.models import F
 
 
 class CartItem(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name='cart_items')
     quantity = models.IntegerField(default=1)
-    created_by = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name='cart_items')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
 
 
 class CartManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().order_by('-created_at')
     
-    def get_user_cart(self, user):
+    def get_user_cart(self, user)-> "Cart":
         cart, _ = self.get_queryset().get_or_create(user=user)
         return cart
     
@@ -22,15 +23,30 @@ class CartManager(models.Manager):
         cart = self.get_user_cart(user)
         return cart.items.all()
     
-    def add_to_cart(self, user, product, quantity):
-        cart = self.get_user_cart(user)
-        cart_item, created = CartItem.objects.get_or_create(product=product)
-        if not created:
-            cart_item.quantity += quantity
+    def add_to_cart(self, user: User, product: Product, quantity=1):
+        user_cart = self.get_user_cart(user)
+
+        # if item exists and is already in cart, update quantity
+        is_in_cart = user_cart.items.filter(product=product).exists()
+        if is_in_cart:
+            # get cart item and update quantity. we can use F() to avoid race conditions
+            cart_item = user_cart.items.get(product=product)
+            cart_item.quantity = F('quantity') + quantity
             cart_item.save()
-        cart.items.add(cart_item)
-        return cart
+            return user_cart
+
+        cart_item = CartItem.objects.create(product=product, quantity=quantity, created_by=user)
+        user_cart.items.add(cart_item)
+        user_cart.save()
+        return user_cart
+    
+    def contains_cart_item(self, user: User, cart_item_id: str):
+        user_cart = self.get_user_cart(user)
+        return user_cart.items.filter(id=cart_item_id).exists()
+   
 
 class Cart(BaseModel):
     items = models.ManyToManyField(CartItem, related_name='carts')
-    user = models.OneToOneField("accounts.User",, on_delete=models.CASCADE, related_name='cart')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    objects: CartManager = CartManager()
+
--- augment-store/server/core/settings.py
+++ augment-store/server/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'mptt',
     'products',
     'storage',
+    'carts',
 ]
 
 MIDDLEWARE = [
