f26f7b3c403cfe59f71157041bc0e7fb69200a77
sagelite <sageliteoff@gmail.com>
2025-11-05 22:49:50 +0000

    SCRUM-34 - create an endpoint for getting order by id

--- augment-store/server/checkout/urls.py
+++ augment-store/server/checkout/urls.py
@@ -6,5 +6,6 @@ app_name = "checkout"
 urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order_list'),
     path('orders/create/', CreateOrderView.as_view(), name='create_order'),
+    path('orders/<uuid:pk>/', RetrieveOrderView.as_view(), name='retrieve_order'),
 ]
 
