42c587cf0a57c0dd81ccc5d2e2a6b2a548d23321
Swapnil <swapdewalkar@gmail.com>
2025-11-06 23:35:31 +0530

    feat: Add tests for merchant product list view and setup for products

--- augment-store/server/merchant/tests.py
+++ augment-store/server/merchant/tests.py
@@ -30,3 +30,44 @@ class MerchantBrandListViewTests(TestCase):
         self.assertEqual(len(response.data['results']), 3)
         values =[response.data['results'][i]["name"] for i in range(len(response.data['results']))]
         self.assertEqual(set(values), set(self.product_brand_names))
+
+
+class MerchantProductListViewTests(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.merchant_id = uuid.uuid4()
+        self.merchant_id_no_products = uuid.uuid4()
+        self.product_names = ["Nike Shoe", "Adidas Shoe", "Puma Shoe"]
+        self.merchant = UserFactory(
+            id=self.merchant_id,
+            email="merchant@demo.com",
+            password="testpass123",
+            is_active=True,
+            role=User.Role.MERCHANT
+        )
+        self.merchant_no_products = UserFactory(
+            id=self.merchant_id_no_products,
+            email="merchant_no_products@demo.com",
+            password="testpass123",
+            is_active=True,
+            role=User.Role.MERCHANT
+        )
+        self.brand = ProductBrandFactory(created_by=self.merchant, name="Nike")
+        self.product_1 = ProductFactory(created_by=self.merchant, name="Nike Shoe")
+        self.product_2 = ProductFactory(created_by=self.merchant, name="Adidas Shoe")
+        self.product_3 = ProductFactory(created_by=self.merchant, name="Puma Shoe")
+
+    def test_merchant_product_list_view(self):
+        url = reverse("v1:merchant:merchant_product_list", kwargs={"pk": str(self.merchant_id)})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 3)
+        values =[response.data['results'][i]["name"] for i in range(len(response.data['results']))]
+        self.assertEqual(set(values), set(self.product_names))
+
+    def test_merchant_product_list_view(self):
+        url = reverse("v1:merchant:merchant_product_list", kwargs={"pk": str(self.merchant_id_no_products)})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 0)
