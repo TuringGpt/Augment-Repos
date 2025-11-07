12cac42d4e8638f25925c875e06587cf23d1b84d
sagelite <sageliteoff@gmail.com>
2025-11-07 03:09:34 +0000

    SCRUM-30 - add limit to filter to the ProductFilter to allow fronted to limit result

--- augment-store/server/products/filters.py
+++ augment-store/server/products/filters.py
@@ -9,6 +9,10 @@ class ProductFilter(filters.FilterSet):
     category = filters.CharFilter(field_name="category__slug")
     brand = filters.CharFilter(field_name="brand__name")
     quantity = filters.RangeFilter()
+    limit = filters.NumberFilter(field_name="limit", method="limit_filter", max_value=100, min_value=1)
+
+    def limit_filter(self, queryset, name, value):
+        return queryset[:value]
     class Meta:
         model = Product
         fields = ["category", "brand", "rating", "price", "quantity" ]
