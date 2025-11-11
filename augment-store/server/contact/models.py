99440ea97268adc286edcf535ad2cc91a8d62050
Swapnil <swapdewalkar@gmail.com>
2025-11-11 19:24:51 +0530

    fix(contact): change ContactMessage model to inherit from BaseModel and update URL parameter from 'id' to 'pk'

--- augment-store/server/contact/models.py
+++ augment-store/server/contact/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class ContactMessage(models.Model):
+class ContactMessage(BaseModel):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     message = models.TextField()
--- augment-store/server/contact/urls.py
+++ augment-store/server/contact/urls.py
@@ -4,6 +4,6 @@ from .views import CreateContactView, ContactListView, ContactDetailView
 urlpatterns = [
     path('', ContactListView.as_view(), name='contact_list_create'),
     path('create/', CreateContactView.as_view(), name='create_contact'),
-    path('<uuid:id>/', ContactDetailView.as_view(), name='contact_detail'),
+    path('<uuid:pk>/', ContactDetailView.as_view(), name='contact_detail'),
 ]
 
