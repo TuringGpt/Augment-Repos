a2cbcdae681639f4fb695d01d5160e869c217e00
Swapnil <swapdewalkar@gmail.com>
2025-11-11 19:17:17 +0530

    feat(contact): add URL pattern for retrieving contact details

--- augment-store/server/contact/urls.py
+++ augment-store/server/contact/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateContactView, ContactListView
+from .views import CreateContactView, ContactListView, ContactDetailView
 
 urlpatterns = [
     path('', ContactListView.as_view(), name='contact_list_create'),
     path('create/', CreateContactView.as_view(), name='create_contact'),
+    path('<uuid:id>/', ContactDetailView.as_view(), name='contact_detail'),
 ]
 
