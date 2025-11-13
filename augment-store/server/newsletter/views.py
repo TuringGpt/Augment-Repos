a69d2c7af058740e0cd0df823c25f7154d161cda
Swapnil <swapdewalkar@gmail.com>
2025-11-13 22:45:48 +0530

    ADDED BASE CLASS

--- augment-store/server/newsletter/views.py
+++ augment-store/server/newsletter/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Newsletter
+from .serializers import NewsletterSerializer
 
 # Create your views here.
-class NewsletterView:
-    pass
+class BaseNewsletterView:
+    serializer_class = NewsletterSerializer
+
+    def get_queryset(self):
+        return Newsletter.objects.all().order_by('-created_at')
+
+class NewsletterView(BaseNewsletterView, ListAPIView):
+    serializer_class = NewsletterSerializer
+
+    
