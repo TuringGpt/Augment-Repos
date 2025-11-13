a7d40bc6408bbb74766a145b9a3cb2bf81e07971
Swapnil <swapdewalkar@gmail.com>
2025-11-13 23:24:59 +0530

    ADDED other apis

--- augment-store/server/newsletter/views.py
+++ augment-store/server/newsletter/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Newsletter
-from .serializers import NewsletterSerializer
+from .serializers import NewsletterSerializer, SubscribeNewsletterSerializer, UnsubscribeNewsletterSerializer
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
@@ -15,3 +15,16 @@ class NewsletterView(BaseNewsletterView, ListAPIView):
     serializer_class = NewsletterSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Newsletter.objects.filter(is_active=True).order_by('-created_at')
+
+class SubscribeNewsletterView(BaseNewsletterView, CreateAPIView):
+    serializer_class = SubscribeNewsletterSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class UnsubscribeNewsletterView(BaseNewsletterView, RetrieveUpdateDestroyAPIView):
+    serializer_class = UnsubscribeNewsletterSerializer
+    permission_classes = [IsAuthenticated]
