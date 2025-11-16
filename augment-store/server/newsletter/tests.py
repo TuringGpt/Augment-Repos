92da2a5197f4f0b1e69f1139d20bd54df0cb1da0
Swapnil <swapdewalkar@gmail.com>
2025-11-16 23:08:53 +0530

    fixed comments

--- augment-store/server/newsletter/tests.py
+++ augment-store/server/newsletter/tests.py
@@ -1,43 +1,34 @@
 from core.tests import BaseAPITestCase
 from django.urls import reverse
 from accounts.factory import UserFactory
+import uuid
 
 # Create your tests here.
 class NewsletterTests(BaseAPITestCase):
 
     def setUp(self):
         super().setUp()
+        self.id = uuid.uuid4()
         self.user = UserFactory(
+            id=self.id,
+            first_name="Test",
             email="test@example.com",
             password="testpassword",
             is_active=True,
         )
     
     def test_subscribe_newsletter(self):
-        url = reverse("v1:subscribe_newsletter")
-        payload = {
-            "email": "test@example.com",
-        }
-        response = self.client.post(url, payload)
+        url = reverse("v1:create_newsletter")
+        self.authenticated_client.force_authenticate(user=self.user)
+        response = self.authenticated_client.get(url)
         self.assertEqual(response.status_code, 201)
 
     def test_unsubscribe_newsletter(self):
-        url = reverse("v1:unsubscribe_newsletter")
-        payload = {
-            "email": "test@example.com",
-        }
-        response = self.client.post(url, payload)
-        self.assertEqual(response.status_code, 200 )
-        
-    def test_list_newsletter(self):
-        # GIVEN an authenticated user exists
-        # WHEN we make a get request to list newsletter
-        url = reverse("v1:newsletter")
+        url = reverse("v1:unsubscribe_newsletter", kwargs={"pk": str(self.id)})
         self.authenticated_client.force_authenticate(user=self.user)
         response = self.authenticated_client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 0)
-        return
+        self.assertEqual(response.status_code, 200 )
+        
     
     def test_list_newsletter_unauthenticated(self):
         url = reverse("v1:newsletter")
