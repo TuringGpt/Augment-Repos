b34b137455944e34c3eb8a94c31567d604f09dea
Swapnil <swapdewalkar@gmail.com>
2025-11-18 21:18:56 +0530

    replaced faker with sequence

--- augment-store/server/newsletter/factory.py
+++ augment-store/server/newsletter/factory.py
@@ -1,9 +1,9 @@
-from factory import Faker
+from factory import Sequence
 
 from factory.django import DjangoModelFactory
 
 class NewsletterFactory(DjangoModelFactory):
-    email = Faker("test@example.com")
+    email = Sequence(lambda n: f"newsletter{n}@example.com")
 
     class Meta:
         model = "newsletter.Newsletter"
