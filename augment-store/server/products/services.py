fa608d49602086741629390b5faa8ddc3fde04b8
sagelite <sageliteoff@gmail.com>
2025-11-19 23:54:43 +0000

    SCRUM-91 - add service for querying featured products

---
+++ augment-store/server/products/services.py
@@ -0,0 +1,10 @@
+
+from accounts.models import User
+
+class ProductService:
+    
+    def get_featured_products(self):
+        pass
+
+    def recommend_products_for_user(self, user: User):
+        pass
