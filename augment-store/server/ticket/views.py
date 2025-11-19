f8f01be1fd730743878c2dd56310790b2b24dec9
Swapnil Dewalkar <swapnil.d@turing.com>
2025-11-19 22:45:49 +0530

    added Views base class

--- augment-store/server/ticket/views.py
+++ augment-store/server/ticket/views.py
@@ -1,46 +1,47 @@
 from django.shortcuts import render
 from .models import Ticket, Comment
 from .serializers import TicketListSerializer, TicketCreateSerializer, TicketUpdateSerializer, TicketDetailSerializer, CommentSerializer, CommentCreateSerializer, CommentUpdateSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
 
 # Create your views here.
-class TicketListView:
+class TicketListView(ListAPIView):
     serializer_class = TicketListSerializer
 
     def get_queryset(self):
         return Ticket.objects.all().order_by('-created_at')
     
 
-class TicketCreateView:
+class TicketCreateView(CreateAPIView):
     serializer_class = TicketCreateSerializer
 
     def perform_create(self, serializer):
         serializer.save(reporter=self.request.user)
 
-class TicketDetailView:
+class TicketDetailView(RetrieveAPIView):
     serializer_class = TicketDetailSerializer
 
     def get_queryset(self):
         return Ticket.objects.all()
 
-class TicketUpdateView:
+class TicketUpdateView(RetrieveUpdateDestroyAPIView):
     serializer_class = TicketUpdateSerializer
 
     def get_queryset(self):
         return Ticket.objects.all()
     
-class CommentListView:
+class CommentListView(ListAPIView):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
         return Comment.objects.all().order_by('-created_at')
     
-class CommentCreateView:
+class CommentCreateView(CreateAPIView):
     serializer_class = CommentCreateSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class CommentUpdateView:
+class CommentUpdateView(RetrieveUpdateDestroyAPIView):
     serializer_class = CommentUpdateSerializer
 
     def get_queryset(self):
