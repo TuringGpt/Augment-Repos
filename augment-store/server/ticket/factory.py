663afdddbadd4e3dd100a291054d61b4e7599b3f
Swapnil Dewalkar <swapnil.d@turing.com>
2025-11-20 23:53:00 +0530

    added factory

---
+++ augment-store/server/ticket/factory.py
@@ -0,0 +1,22 @@
+from factory.django import DjangoModelFactory
+from factory import Faker, SubFactory
+
+
+class TicketFactory(DjangoModelFactory):
+    title = Faker("sentence", nb_words=5)
+    description = Faker("text", max_nb_chars=500)
+    status = Faker("word")
+    priority = Faker("word")
+    assignee = SubFactory(UserFactory)
+    reporter = SubFactory(UserFactory)
+
+    class Meta:
+        model = "ticket.Ticket"
+
+class CommentFactory(DjangoModelFactory):
+    ticket = SubFactory(TicketFactory)
+    user = SubFactory(UserFactory)
+    content = Faker("text", max_nb_chars=500)
+
+    class Meta:
+        model = "ticket.Comment"
