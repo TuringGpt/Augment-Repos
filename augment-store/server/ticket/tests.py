74d935b4fe4d55c6c7a04e58cc065620c8668994
Swapnil Dewalkar <swapnil.d@turing.com>
2025-11-21 23:54:17 +0530

    Refactor ticket test assertions to use direct ID references

--- augment-store/server/ticket/tests.py
+++ augment-store/server/ticket/tests.py
@@ -57,34 +57,34 @@ class TicketTests(BaseAPITestCase):
 
         # THEN we should get a 200 response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertGreaterEqual(len(response.data), 1)
+        self.assertGreaterEqual(len(response.data.get("results", [])), 1)
     
     def test_ticket_detail(self):
         # GIVEN an authenticated user exists
         # WHEN we make a get request to retrieve ticket details
-        url = reverse("v1:ticket:ticket_detail", args=[str(self.ticket.id)])
+        url = reverse("v1:ticket:ticket_detail", args=[self.ticket.id])
         response = self.authenticated_client.get(url)
 
         # THEN we should get a 200 response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["id"], str(self.ticket.id))  
+        self.assertEqual(response.data["id"], self.ticket.id)  
         self.assertEqual(response.data["title"], "Test Title")  
         self.assertEqual(response.data["description"], "Test Description")  
         self.assertEqual(response.data["status"], "Test Status")  
         self.assertEqual(response.data["priority"], "Test Priority")  
-        self.assertEqual(response.data["assignee"], str(self.user.id))  
-        self.assertEqual(response.data["reporter"], str(self.user.id))  
+        self.assertEqual(response.data["assignee"], self.user.id)  
+        self.assertEqual(response.data["reporter"], self.user.id)  
 
     def test_update_ticket(self):
         # GIVEN an authenticated user exists
         # WHEN we make a put request to update ticket details
-        url = reverse("v1:ticket:update_ticket", args=[str(self.ticket.id)])
+        url = reverse("v1:ticket:update_ticket", args=[self.ticket.id])
         payload = {
             "title": "Updated Title",
             "description": "Updated Description",
             "status": "Updated Status",
             "priority": "Updated Priority",
-            "assignee": str(self.user2.id),
+            "assignee": self.user2.id,
         }
         response = self.authenticated_client.put(url, payload)
 
@@ -94,4 +94,4 @@ class TicketTests(BaseAPITestCase):
         self.assertEqual(response.data["description"], "Updated Description")  
         self.assertEqual(response.data["status"], "Updated Status")  
         self.assertEqual(response.data["priority"], "Updated Priority")  
-        self.assertEqual(response.data["assignee"], str(self.user2.id))
+        self.assertEqual(response.data["assignee"], self.user2.id)
