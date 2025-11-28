a4bbd6375d8a4af66e2bf3e39ff4a785f99c8843
sagelite <sageliteoff@gmail.com>
2025-11-28 13:55:15 +0000

    SCRUM-42 - remove unused imports

--- augment-store/server/notifications/views.py
+++ augment-store/server/notifications/views.py
@@ -1,7 +1,5 @@
 from rest_framework.generics import ListAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
 from .models import Notification
 from .serializers import NotificationListSerializer, UpdateNotificationSerializer
 
