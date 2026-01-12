55a05165799b4861969f348589dc7dfff5cfc9fe
Swapnil <164381611+swapnil-turing@users.noreply.github.com>
2026-01-12 22:21:27 +0530

    feat: Optimize merchant orders view (#342)
    * Optimize merchant orders view
    
    * Fix MerchantOrdersListView mixin usage and distinct duplicate results
    
    * Fix MerchantOrdersListView runtime crash by adding base queryset

--- augment-store/server/merchant/views.py
+++ augment-store/server/merchant/views.py
@@ -3,6 +3,7 @@ from checkout.models import Order
 from rest_framework.generics import ListAPIView
 from .serializers import MerchantBrandSerializer, MerchantProductSerializer, MerchantOrdersSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from core.optimization import AutoOptimizeMixin
 
 class MerchantBrandListView(ListAPIView):
     serializer_class = MerchantBrandSerializer
@@ -22,9 +23,19 @@ class MerchantProductListView(ListAPIView):
         # Return a QuerySet of all products from brands created by this merchant
         return Product.objects.filter(brand__created_by=object_id)
 
-class MerchantOrdersListView(ListAPIView):
+class MerchantOrdersListView(AutoOptimizeMixin, ListAPIView):
     serializer_class = MerchantOrdersSerializer
     permission_classes = [IsAuthenticated]
+    queryset = Order.objects.all()
+    auto_prefetch_related = [
+        'items',
+        'items__product',
+        'items__product__brand',
+        'items__product__category',
+        'items__product__images'
+    ]
 
     def get_queryset(self):
-        return Order.objects.filter(items__cart_item__product__brand__created_by=self.request.user).distinct()
+        return super().get_queryset().filter(
+            items__cart_item__product__brand__created_by=self.request.user
+        ).distinct()
