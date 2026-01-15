d8012f8fc7e959a6173bc0f8f914faa178f1694f
Swapnil <swapnil.d@turing.com>
2026-01-15 11:53:18 +0530

    Add custom manager to Currency model

--- augment-store/server/currencies/models.py
+++ augment-store/server/currencies/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from core.models import BaseModel
 
 
+class CurrencyManager(models.Manager):
+    def get_by_code(self, code: str):
+        return self.get(code=code)
+
+
 class Currency(BaseModel):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255)
     symbol = models.CharField(max_length=255)
+
+    objects = CurrencyManager()
