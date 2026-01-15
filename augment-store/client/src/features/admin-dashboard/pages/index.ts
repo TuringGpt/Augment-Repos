a0bd48870db86a99a88f4e88137a7311686f546e
Ahmad Jinadu <ahmad.j@turing.com>
2026-01-15 08:17:09 +0100

    added the brands page

--- augment-store/client/src/features/admin-dashboard/pages/index.ts
+++ augment-store/client/src/features/admin-dashboard/pages/index.ts
@@ -3,4 +3,5 @@ export { default as AdminProductsPage } from './AdminProductsPage'
 export { default as AdminUsersPage } from './AdminUsersPage'
 export { default as AdminAllProductsPage } from './AdminAllProductsPage'
 export { default as AdminCategoriesPage } from './AdminCategoriesPage'
+export { default as AdminBrandsPage } from './AdminBrandsPage'
 
