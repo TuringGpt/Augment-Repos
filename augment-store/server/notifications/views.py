50247d260098ebb0475d9ad1cb125d61ac1b270d
Swapnil <swapnil.d@turing.com>
2026-01-23 00:16:12 +0530

    Add AutoOptimizeMixin to notifications views

--- augment-store/server/notifications/views.py
+++ augment-store/server/notifications/views.py
@@ -4,11 +4,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Notification
 from .serializers import MarkAsReadSerializer, NotificationListSerializer, UpdateNotificationSerializer
+from core.optimization import AutoOptimizeMixin
 
 
-class BaseNotificationView:
+class BaseNotificationView(AutoOptimizeMixin):
     serializer_class = NotificationListSerializer
     permission_classes = [IsAuthenticated]
+    queryset = Notification.objects.all()
+    auto_select_related = ['user']
 
     def get_queryset(self):
         return Notification.objects.get_user_notifications(self.request.user)
