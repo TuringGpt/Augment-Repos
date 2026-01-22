5b148035b3083fc49e037a8c0c405331ab0f5e19
Swapnil <swapnil.d@turing.com>
2026-01-22 23:24:10 +0530

    Fix invalid auto_select_related and restore permission_classes

--- augment-store/server/contact/views.py
+++ augment-store/server/contact/views.py
@@ -8,13 +8,13 @@ from core.optimization import AutoOptimizeMixin
 class BaseContactView(AutoOptimizeMixin):
     serializer_class = ContactMessageSerializer
     queryset = ContactMessage.objects.all()
-    auto_select_related = ['user']
 
     def get_queryset(self):
         return super().get_queryset().order_by('-created_at')
     
 class ContactListView(BaseContactView, ListAPIView):
     serializer_class = ContactMessageSerializer
+    permission_classes = [IsAuthenticated, hasAdminRole]
 
 class CreateContactView(BaseContactView, CreateAPIView):
     serializer_class = ContactMessageSerializer
