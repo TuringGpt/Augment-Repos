fd53c2360e8d8161281e41cbfbbd4478798e94cc
Ahmad Jinadu <ahmad.j@turing.com>
2026-02-05 06:42:31 +0100

    added label

--- augment-store/client/src/components/Footer.tsx
+++ augment-store/client/src/components/Footer.tsx
@@ -506,6 +506,7 @@ const Footer = () => {
                     fullWidth
                     size="small"
                     type="email"
+                    label={t('footer.emailLabel')}
                     placeholder={t('footer.enterEmail')}
                     value={email}
                     onChange={handleEmailChange}
