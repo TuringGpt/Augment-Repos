c01204d450f7ece7aafbeb477f8937a7463ee40b
Ahmad Jinadu <ahmad.j@turing.com>
2026-02-09 07:03:41 +0100

    used the useTranslation hook

--- augment-store/client/src/features/info/shipping/components/ShippingPage.tsx
+++ augment-store/client/src/features/info/shipping/components/ShippingPage.tsx
@@ -10,7 +10,7 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from '@hooks/useTranslation'
 import { CONTACT_INFO } from '@constants/index'
 
 const ShippingPage = () => {
