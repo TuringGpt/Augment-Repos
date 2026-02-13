644945af4fe3f98e3c905b8a415a09a2db1dad4c
Swapnil <swapnil.d@turing.com>
2026-02-13 11:27:55 +0530

    fix(currency): add code normalization and data cleanup migration

--- augment-store/server/currencies/models.py
+++ augment-store/server/currencies/models.py
@@ -6,3 +6,7 @@ class Currency(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     code = models.CharField(max_length=255, unique=True)
     symbol = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        self.code = self.code.upper().strip()
+        super().save(*args, **kwargs)
