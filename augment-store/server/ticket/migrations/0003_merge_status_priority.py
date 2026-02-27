2feb3a37cd83f0b0c6586432ff26f7e5368d99b2
Swapnil <swapnil.d@turing.com>
2026-02-27 12:16:50 +0530

    fix: Add merge migration for conflicting status and priority migrations

---
+++ augment-store/server/ticket/migrations/0003_merge_status_priority.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ticket', '0002_alter_ticket_priority'),
+        ('ticket', '0002_alter_ticket_status'),
+    ]
+
+    operations = [
+    ]
