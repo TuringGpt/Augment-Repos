4f06f5197b2d3e3db2e15b10030376c07360ea36
Swapnil <swapnil.d@turing.com>
2026-03-10 23:24:57 +0530

    fix: Alias DRF ValidationError to avoid shadowing Django ValidationError

--- augment-store/server/checkout/views.py
+++ augment-store/server/checkout/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 
 from rest_framework import status
+from rest_framework.exceptions import ValidationError as DRFValidationError
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -58,10 +59,9 @@ class OrderListView(BaseOrderView, ListAPIView):
         queryset = super().get_queryset()
         status_filter = self.request.query_params.get('status')
         if status_filter:
-            from rest_framework.exceptions import ValidationError
             valid_statuses = [s[0] for s in Order.OrderStatus.CHOICES]
             if status_filter not in valid_statuses:
-                raise ValidationError({'status': f'Invalid status. Valid choices are: {valid_statuses}'})
+                raise DRFValidationError({'status': f'Invalid status. Valid choices are: {valid_statuses}'})
             queryset = queryset.filter(status=status_filter)
         return queryset
 
