44f6f87aee89cfdd29f1ba58bf2ad3b2568a7228
Swapnil <swapnil.d@turing.com>
2026-03-12 10:54:29 +0530

    fix: Use queryset.count() instead of len() for DB-level product count

--- augment-store/server/carts/views.py
+++ augment-store/server/carts/views.py
@@ -62,7 +62,7 @@ class ListWishListProductsView(AutoOptimizeMixin, BaseWishlistView, ListAPIView)
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        product_count = len(queryset)  # bug: should use queryset.count() to avoid loading all objects
+        product_count = queryset.count()
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
