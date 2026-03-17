2186f5aa517dec13cec378ba9fea0a958acd93fb
Swapnil <swapnil.d@turing.com>
2026-03-18 00:03:40 +0530

    fix(currencies): Use UUID path converter instead of int

--- augment-store/server/currencies/urls.py
+++ augment-store/server/currencies/urls.py
@@ -6,5 +6,5 @@ app_name = 'currencies'
 urlpatterns = [
     path('', CurrencyListView.as_view(), name='currency_list'),
     path('create/', CreateCurrencyView.as_view(), name='create_currency'),
-    path('<int:pk>/', AdminCurrencyUpdateDeleteView.as_view(), name='admin_currency_update_delete'),
+    path('<uuid:pk>/', AdminCurrencyUpdateDeleteView.as_view(), name='admin_currency_update_delete'),
 ]
