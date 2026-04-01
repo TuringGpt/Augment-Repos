60d3f13eb0a83d8497a59e57d8a98c1e9252e90a
Swapnil <swapnil.d@turing.com>
2026-04-01 17:12:21 +0530

    feat(storage): implement AdminFileListView with caching and optimization

--- augment-store/server/storage/views.py
+++ augment-store/server/storage/views.py
@@ -1,12 +1,18 @@
 
 
-
-from rest_framework.generics import CreateAPIView
-
-from .serializers import StartDirectFileUploadSerializer, DirectLocalFileUploadSerializer
-from .serializers import FinishFileUploadSerializer
-
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import hasAdminRole
+from core.optimization import AutoOptimizeMixin
+from core.service import CachedListMixin, BaseCacheService
+
+from .models import File
+from .serializers import (StartDirectFileUploadSerializer, DirectLocalFileUploadSerializer, 
+                         FinishFileUploadSerializer, FileListSerializer)
+
+class AdminFileCacheService(BaseCacheService):
+    OBJECT_NAME = "admin_file"
+    VERSION = 1
 
 
 class StartDirectFileUpload(CreateAPIView):
@@ -22,5 +28,18 @@ class DirectLocalFileUpload(CreateAPIView):
 class FinishDirectFileUploadFinish(CreateAPIView):
     serializer_class = FinishFileUploadSerializer
     permission_classes = [IsAuthenticated]
+
+
+class AdminFileListView(CachedListMixin, AutoOptimizeMixin, ListAPIView):
+    """
+    Admin-only view to list all files in the system.
+    """
+    serializer_class = FileListSerializer
+    permission_classes = [IsAuthenticated, hasAdminRole]
+    cache_service_class = AdminFileCacheService
+    queryset = File.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False).order_by('-created_at')
 
 
