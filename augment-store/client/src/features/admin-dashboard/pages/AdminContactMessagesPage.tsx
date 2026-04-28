77d52a12cd4482e3504587d2c33c2deb9668c42c
Ahmad Jinadu <ahmad.j@turing.com>
2026-04-28 14:35:52 +0100

    removed state updater function

--- augment-store/client/src/features/admin-dashboard/pages/AdminContactMessagesPage.tsx
+++ augment-store/client/src/features/admin-dashboard/pages/AdminContactMessagesPage.tsx
@@ -399,13 +399,9 @@ const AdminContactMessagesPage = () => {
         // If the deleted contact was being viewed in the drawer, close the drawer
         // Check the current drawer selection at completion time to avoid closing
         // unexpectedly if the user has opened a different contact
-        setSelectedContact((currentContact) => {
-          if (currentContact?.id === contactId) {
-            handleCloseDetailsDrawer()
-            return null
-          }
-          return currentContact
-        })
+        if (selectedContact?.id === contactId) {
+          handleCloseDetailsDrawer()
+        }
       }
     } catch (error) {
       // Error is already handled by the store and stored in deleteErrors
