b676e7fda7e472dbbdabda6ef5c422de7c204cd8
Swapnil D <swapnil.d@turing.com>
2026-05-05 10:05:27 +0530

    fix: Make product ownership read-only

--- augment-store/server/products/serializers.py
+++ augment-store/server/products/serializers.py
@@ -121,6 +121,7 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+        read_only_fields = ("created_by",)
 
 class SearchQueryListSerializer(serializers.ModelSerializer):
     user = UserListSerializer(read_only=True)
