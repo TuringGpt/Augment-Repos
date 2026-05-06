74789e66ccb35c710a844d2f3c9cd768648e345e
Swapnil <164381611+swapnil-turing@users.noreply.github.com>
2026-05-06 21:59:20 +0530

    fix: invalidate product search cache (#635)
    * Fix product search cache invalidation
    
    * Address product search cache review

--- augment-store/server/products/views.py
+++ augment-store/server/products/views.py
@@ -242,6 +242,7 @@ class ProductUpdateDeleteView(CacheInvalidatorMixin, BaseProductView, RetrieveUp
     def invalidate_cache(self):
         super().invalidate_cache()
         FeaturedProductCacheService().clear_namespace()
+        ProductSearchCacheService().clear_namespace()
 
     def get_permissions(self):
         if self.request.method in SAFE_METHODS:
