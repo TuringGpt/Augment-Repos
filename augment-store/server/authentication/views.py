286b1ca0471df1a2c6a1e3dcb5e25593fef8bc85
Swapnil D <swapnil.d@turing.com>
2026-05-21 11:03:21 +0530

    fix: restore auth permissions

--- augment-store/server/authentication/views.py
+++ augment-store/server/authentication/views.py
@@ -18,7 +18,7 @@ class LoginView(CreateAPIView):
         return LoginSerializer
 
 class LogoutView(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         return Response({
@@ -28,12 +28,12 @@ class LogoutView(APIView):
 
 class RefreshTokenView(CreateAPIView):
     serializer_class = RefreshTokenSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class ResetPasswordView(CreateAPIView):
     serializer_class = ForgotPasswordSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
