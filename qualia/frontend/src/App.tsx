27b5c62f57fc17affcb87c0f2fdd5eb6a90252c4
arkmadj <ahmadjinadu@gmail.com>
2026-05-21 10:26:07 +0100

    feat: setup up frontend using Vite and TypeScript

---
+++ qualia/frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id='center'>
+        <h1>Welcome to Quali</h1>
+        <h3>
+          Quali is purpose-built QA platform that gives us everything Google
+          Forms does, plus an AI layer that turns all our reviewer submissions
+          into a single consolidated report automatically.{" "}
+        </h3>
+        <button>Sign in</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
