57f914ab19780135da10a5f7751be1fae67ba558
ahmadJT <166782331+ahmadJT@users.noreply.github.com>
2026-05-22 15:02:20 +0100

    feat: setup up frontend using Vite and TypeScript (#684)
    * feat: setup up frontend using Vite and TypeScript
    
    * fix grammatical error
    
    * updated tab title
    
    * removed boilerplate code
    
    * update home page app name
    
    * remove dead CSS from App.css
    
    * remove unused CSS variables

---
+++ qualia/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <section id='center'>
+        <h1>Welcome to Qualia</h1>
+        <h3>
+          Qualia is a purpose-built QA platform that gives us everything Google
+          Forms does, plus an AI layer that turns all our reviewer submissions
+          into a single consolidated report automatically.
+        </h3>
+        <button>Sign in</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
