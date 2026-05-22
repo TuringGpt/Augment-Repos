ada295a60595a3228c6efc6ee20bb583c662dda5
Ahmad Jinadu <ahmad.j@turing.com>
2026-05-22 19:52:32 +0100

    use the correct provider component

--- qualia/frontend/src/main.tsx
+++ qualia/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClientProviders } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import App from './App.tsx'
@@ -8,9 +8,9 @@ import { queryClient } from './lib/queryClient'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProviders client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProviders>
+    </QueryClientProvider>
   </StrictMode>,
 )
