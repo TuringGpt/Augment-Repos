bc665de2dd0f4177a0db28c8d03bd606bb11640e
Ahmad Jinadu <ahmad.j@turing.com>
2026-05-26 08:10:22 +0100

    fix button import

--- qualia/frontend/src/pages/SignIn.tsx
+++ qualia/frontend/src/pages/SignIn.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./SignIn.css";
-import { Button } from "@/components/ui/buttons";
+import { Button } from "@/components/ui/button";
 
 interface SignInFormData {
   email: string;
