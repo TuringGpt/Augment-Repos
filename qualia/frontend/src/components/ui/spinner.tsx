c73ce6aaa91cf85697ba2214538a296f413120f3
Ahmad Jinadu <ahmad.j@turing.com>
2026-05-27 09:29:30 +0100

    add role

--- qualia/frontend/src/components/ui/spinner.tsx
+++ qualia/frontend/src/components/ui/spinner.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import { cn } from "@/lib/utils"
 import { Loader2Icon } from "lucide-react"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({ className, role = "status", ...props }: React.ComponentProps<"svg">) {
   return (
-    <Loader2Icon role="status" className={cn("size-4 animate-spin", className)} {...props} />
+    <Loader2Icon {...props} className={cn("size-4 animate-spin", className)} role={role} />
   )
 }
 
