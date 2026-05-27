4e9111c70329ec7c891f501d45a2134d4a41888f
Ahmad Jinadu <ahmad.j@turing.com>
2026-05-27 10:34:09 +0100

    fix z index

--- qualia/frontend/src/components/ui/navigation-menu.tsx
+++ qualia/frontend/src/components/ui/navigation-menu.tsx
@@ -141,7 +141,7 @@ function NavigationMenuIndicator({
     <NavigationMenuPrimitive.Indicator
       data-slot="navigation-menu-indicator"
       className={cn(
-        "top-full z-1 flex h-1.5 items-end justify-center overflow-hidden data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:animate-in data-[state=visible]:fade-in",
+        "top-full z-10 flex h-1.5 items-end justify-center overflow-hidden data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:animate-in data-[state=visible]:fade-in",
         className
       )}
       {...props}
