5ec2a74cddd5f3588b26dfc80b9e61c66df8d3f9
ahmadJT <166782331+ahmadJT@users.noreply.github.com>
2026-05-29 11:52:25 +0100

    feat(Register): create the register page (#729)
    * feat(Register): create the register page
    
    * update sign in flow
    
    * clean timeout
    
    * update password type
    
    * update sign in link
    
    * add password validation
    
    * improve error handling
    
    * clear timeout

--- qualia/frontend/src/App.tsx
+++ qualia/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
