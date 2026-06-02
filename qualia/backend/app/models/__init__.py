ed2d002d6e9007fc4608f6e50c47bccba1e5bc6c
Swapnil <swapnil.d@turing.com>
2026-06-02 21:07:46 +0530

    feat: Create submission answer model

--- qualia/backend/app/models/__init__.py
+++ qualia/backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.section import Section
 from app.models.form_cycle import FormCycle, FormCycleStatus
 from app.models.submission import Submission, SubmissionStatus
+from app.models.submission_answer import SubmissionAnswer
 from app.models.form_assignment import FormAssignment
 from app.models.question import Question, QuestionType
 from app.models.user import Role, User
