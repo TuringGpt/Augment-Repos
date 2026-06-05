29642c9f2a04407a1626dd75a5a4368ec52db650
ahmadJT <166782331+ahmadJT@users.noreply.github.com>
2026-06-05 11:39:25 +0100

    feat(NotFount): add the not found page (#777)

--- qualia/frontend/src/App.tsx
+++ qualia/frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import "./App.css";
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import Register from './pages/Register';
-import ForgotPassword from './pages/ForgotPassword';
-import Dashboard from './pages/Dashboard';
-import { ThemeProvider } from './components/theme-provider';
+import "@/App.css";
+import Home from '@/pages/Home';
+import SignIn from '@/pages/SignIn';
+import Register from '@/pages/Register';
+import ForgotPassword from '@/pages/ForgotPassword';
+import Dashboard from '@/pages/Dashboard';
+import NotFound from '@/pages/NotFound';
+import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Toaster />
       </BrowserRouter>
