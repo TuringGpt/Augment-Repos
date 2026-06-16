c9e31f3c2102b4135b4e4524f98282e2ff14a2c3
Ahmad Jinadu <ahmad.j@turing.com>
2026-06-16 12:55:36 +0100

    rearrange Tailwind classes

--- qualia/frontend/src/components/layouts/DashboardLayout.tsx
+++ qualia/frontend/src/components/layouts/DashboardLayout.tsx
@@ -40,7 +40,7 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 lg:ml-64 pt-14 lg:pt-0 pb-16 lg:pb-0 p-4 sm:p-6 lg:p-8">
+      <main className="flex-1 lg:ml-64 p-4 sm:p-6 lg:p-8 pt-14 lg:pt-0 pb-16 lg:pb-0">
         {children}
       </main>
 
