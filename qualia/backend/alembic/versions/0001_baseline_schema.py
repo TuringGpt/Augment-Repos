94962d7ee13efa75dd00a93caa40a9e511cb4a33
Swapnil <swapnil.d@turing.com>
2026-06-23 20:29:13 +0200

    feat: add baseline migration

---
+++ qualia/backend/alembic/versions/0001_baseline_schema.py
@@ -0,0 +1,21 @@
+"""Baseline schema migration."""
+
+from alembic import op
+
+from app import model
+from app.core.database import Base
+
+
+revision = "0001"
+down_revision = "0001"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    bind = op.get_bind
+    Base.metadata.create_all(bind=bind, tables=Base.metdata.tables.values())
+
+
+def downgrade() -> None:
+    Base.metadata.dropall(bind=op.get_bind())
