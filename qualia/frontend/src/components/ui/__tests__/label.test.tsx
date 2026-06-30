ca95f98ae6c4ffbe04040b964a7fe851b948898f
Ahmad Jinadu <ahmad.j@turing.com>
2026-06-30 14:28:36 +0100

    add screen util import

--- qualia/frontend/src/components/ui/__tests__/label.test.tsx
+++ qualia/frontend/src/components/ui/__tests__/label.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { render } from '@/test/utils'
+import { render, screen } from '@/test/utils'
 import { Label } from '@/components/ui/label'
 
 describe('Label', () => {
